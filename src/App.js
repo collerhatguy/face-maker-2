2044aca9700c476bbc4089b0cbe5fcb4decb620d
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-04-18 12:26:42 -0400

    use reducer works but may not be formatted in the best way

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './styles/App.css';
 import Head from "./components/Head";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div className="App">
       <Head />
+      <Form />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,49 @@
-import React from 'react'
 
+import React, {useReducer, useEffect} from 'react'
+const ACTIONS = {
+    setHair: "set-hair",
+    setHead: "set-head",
+    setEars: "set-ears",
+    setHair: "set-hair",
+    setHair: "set-hair",
+    setHair: "set-hair",
+} 
+function reducer(state, action) {
+    switch (action.type) {
+        case ACTIONS.setHair:
+            return {
+                ...state,
+                hair: {
+                    color: action.payload.color
+                }
+            }
+    }
+
+}
 export default function Form() {
+    const [state, dispatch] = useReducer(reducer, {});
+    const changeHair = (value) => {
+        dispatch({
+            type: ACTIONS.setHair, 
+            payload: {
+                color: value
+            }
+        })
+    }
+    useEffect(() => {
+        console.log(state);
+    }, [state]);
     return (
         <form>
             <feildset>
                 <legend>Hair</legend>
-                <label></label>
+                <label for="hair-color">Color: </label>
+                <input 
+                    type="color"
+                    id="hair-color"
+                    name="hair-color"
+                    onChange={e => {changeHair(e.target.value)}}
+                />
             </feildset>
         </form>
     )
