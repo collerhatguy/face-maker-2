1056a209b8d3831c6465f148f5cf98d5f071f9b8
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-04-19 11:39:10 -0400

    finished stlying the whole page

--- src/components/Head.js
+++ src/components/Head.js
@@ -2,57 +2,59 @@ import React from 'react'
 
 export default function Head({face}) {
     return (
-        <div
-            style={face.head} 
-            class="head">
+        <div className="head-container">
             <div
-                style={face.hair} 
-                class="hair">
-                <div 
+                style={face.head} 
+                class="head">
+                <div
                     style={face.hair} 
-                    class="hair-corner"></div>
-            </div>
-            <div 
-                style={face.brows} 
-                class="brows">
-                <div class="brow"></div>
-                <div class="brow"></div>
-            </div>
-            <div 
-                class="eyes">
-                <div 
-                    style={face.eyes}
-                    class="eye">
+                    class="hair">
                     <div 
-                        style={face.eyes.iris}
-                        className="iris"></div>
-                    <div className="pupil"></div>
+                        style={face.hair} 
+                        class="hair-corner"></div>
                 </div>
                 <div 
-                    style={face.eyes}
-                    class="eye">
+                    style={face.brows} 
+                    class="brows">
+                    <div class="brow"></div>
+                    <div class="brow"></div>
+                </div>
+                <div 
+                    class="eyes">
                     <div 
-                        style={face.eyes.iris}
-                        className="iris">
+                        style={face.eyes}
+                        class="eye">
+                        <div 
+                            style={face.eyes.iris}
+                            className="iris"></div>
+                        <div className="pupil"></div>
                     </div>
-                    <div className="pupil"></div>
+                    <div 
+                        style={face.eyes}
+                        class="eye">
+                        <div 
+                            style={face.eyes.iris}
+                            className="iris">
+                        </div>
+                        <div className="pupil"></div>
+                    </div>
+                </div>
+                <div 
+                    style={face.nose}
+                    class="nose"></div>
+                <div 
+                    style={face.mouth}
+                    class="mouth"></div>
+                <div 
+                    class="ears">
+                    <div 
+                        style={face.ears}
+                        class="ear"></div>
+                    <div 
+                        style={face.ears}
+                        class="ear"></div>
                 </div>
             </div>
-            <div 
-                style={face.nose}
-                class="nose"></div>
-            <div 
-                style={face.mouth}
-                class="mouth"></div>
-            <div 
-                class="ears">
-                <div 
-                    style={face.ears}
-                    class="ear"></div>
-                <div 
-                    style={face.ears}
-                    class="ear"></div>
-            </div>
-      </div>
+        </div>
     )
 }
