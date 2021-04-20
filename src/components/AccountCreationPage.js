39dcca8fd2869a472a5cdc66c07f23f120034876
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-04-20 13:32:51 -0400

    not sure how to connect api to form or what url to use

--- src/components/AccountCreationPage.js
+++ src/components/AccountCreationPage.js
@@ -1,18 +1,48 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function AccountCreationPage() {
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+    const options = {
+        method: "POST",
+        body: JSON.stringify({
+            name: name,
+            password: password
+        })
+    }
+    const submitForm = async () => {
+        try {
+            const response = await fetch("https://github.com/collerhatguy/face-maker-2-serve/create-account", options);
+            console.log(response);
+        } catch(err) {
+            console.log(err);
+        }
+    }
     return (
         <div>
-            <form className="account-form">
+            <form
+                method="POST"
+                onSubmit={() => submitForm()} 
+                className="account-form">
                 <fieldset>
                     <legend>Create Account</legend>
                     <label for="name">
                         Name:
-                        <input type="text" name="name" id="name" placeholder="your name"/>
+                        <input 
+                            onChange={e => setName(e.target.value)}
+                            type="text" 
+                            name="name" 
+                            id="name" 
+                            placeholder="your name"/>
                     </label>
                     <label for="password">
                         Password:
-                        <input type="text" name="password" id="password" placeholder="your password"/>
+                        <input 
+                            onChange={e => setPassword(e.target.value)}
+                            type="text" 
+                            name="password" 
+                            id="password" 
+                            placeholder="your password"/>
                     </label>
                     <button type="submit">Submit</button>
                 </fieldset>
