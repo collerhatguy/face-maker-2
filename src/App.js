c8a786db6c4bf577b79ac38399ba68dbd65f6771
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-04-20 12:45:33 -0400

    added link to login page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import AppPage from "./components/AppPage";
 import AccountCreationPage from "./components/AccountCreationPage";
+import LoginPage from "./components/LoginPage";
 
 import './styles/App.css';
 
@@ -14,6 +15,9 @@ function App() {
           <Link to="/create-account">
             Create Account
           </Link>
+          <Link to="/login">
+            Login
+          </Link>
           <Link to="/">
             Home
           </Link>
@@ -22,6 +26,7 @@ function App() {
       <Switch>
           <Route path="/" exact component={AppPage} />
           <Route path="/create-account" component={AccountCreationPage} />
+          <Route path="/login" component={LoginPage} />
       </Switch>
     </Router>
   );
